4ae235b963f2abb9037edaf67de2dd5f2e307cd3
snedmonty <45636059+snedmonty@users.noreply.github.com>
2019-05-13 18:42:56 -0400

    Create helloworldpy.py

---
+++ helloworldpy.py
@@ -0,0 +1,12 @@
+Instructions
+Create two variables called name and country that will hold strings.
+
+Create two variables called age and hourly_wage that will hold integers.
+
+Create a variable called satisfied which will hold a boolean.
+
+Create a variable called daily_wage that will hold the value of hourly_wage multiplied by 8.
+
+Use traditional string concatenation to print the name, country, age, and hourly_wage variables.
+
+With an f-string, print the daily_wage and satisfied variables.
