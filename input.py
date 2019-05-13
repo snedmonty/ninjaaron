44ae1c8857ce9ca6ede61f53ea19723949103af8
snedmonty <45636059+snedmonty@users.noreply.github.com>
2019-05-13 19:26:09 -0400

    Create input.py

---
+++ input.py
@@ -0,0 +1,10 @@
+# Collects the user's input for the prompt "What is your name?"
+name = input("What is your name? ")
+
+# Collects the user's input for the prompt "How old are you?" and converts the string to an integer.
+
+
+
+# Creates three print statements that to respond with the output.
+my name is ......
+I will be ... years next year
