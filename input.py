feb607f05e84580d4749b51fea5e053b8e800680
snedmonty <45636059+snedmonty@users.noreply.github.com>
2019-05-13 19:26:53 -0400

    Update input.py

--- input.py
+++ input.py
@@ -8,3 +8,15 @@ name = input("What is your name? ")
 # Creates three print statements that to respond with the output.
 my name is ......
 I will be ... years next year
+
+
+
+## Basic Variables
+
+## Instructions
+
+* Create two different variables that will take the input of your first name and your neighbor's first name.
+
+* Create two more inputs that will ask how many months each of you has been coding.
+
+* Finally, display a result with both your names and the total amount of months coding.
